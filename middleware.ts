5935cbd283c5cc640fba2cecdf2e7c5e88b4462d
zanjeel <zanjeel123@gmail.com>
2025-03-04 19:35:59 +0000

    cors issues9

--- middleware.ts
+++ middleware.ts
@@ -29,14 +29,13 @@ export function middleware(request: NextRequest) {
         });
     }
 
-    // Create a new response to modify headers properly
-    const response = new NextResponse(null, {
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        },
-    });
+    // Get the response and add CORS headers
+    const response = NextResponse.next();
+    
+    // Add CORS headers to the response
+    response.headers.set('Access-Control-Allow-Origin', '*');
+    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
     console.log('Request processed, continuing to handler');
     return response;
